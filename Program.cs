11f2636ffbf04bf0e531ebbcc89eb54f82fe0eb9
tester4 <tester4@giftrete.com>
2022-02-20 19:18:46 +0000

    add files

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleProgram
+{
+    public class MathUtils
+    {
+        public static double Average(int a, int b)
+        {
+            int result = a + b / 2;
+            return result;
+
+        }
+
+        public static void Main(string[] args)
+        {
+
+           Console.WriteLine(Average(5, 3));
+
+        }
+
+    }
+}
